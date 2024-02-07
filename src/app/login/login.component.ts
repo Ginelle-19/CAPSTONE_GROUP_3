496f01d61af74dcd510fcba79eedb90eb2231e58
agm.dev <101726053+Ginelle-19@users.noreply.github.com>
2024-02-08 03:51:50 +0800

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,19 @@ import { AuthService } from '../services/auth.service';
 import { ActivatedRoute, Router, RouterModule } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { FormsModule } from '@angular/forms';
+import { MatIconModule } from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
-  imports: [RouterModule,
-  FormsModule],
+  imports: [
+    RouterModule,
+    FormsModule,
+    MatIconModule
+  ],
   standalone: true,
 })
 export class LoginComponent {
@@ -38,41 +44,38 @@ export class LoginComponent {
 
   OnLoginClicked() {
     if (!this.termsAgreed) {
-      console.log('Snackbar should appear');
       this.snackbar.open('You must agree to the Terms and Conditions to log in.', 'Close', { duration: 3000 });
       return;
     }
     
     const UserName = this.UserName.nativeElement.value;
     const Password = this.Password.nativeElement.value;
-
-
+  
     this.authService.login(UserName, Password).subscribe(
       (user) => {
         if (user) {
-          alert('Welcome ' + user.FirstName + '. You are logged in.');
-          
-  
-          // Check user's access level and navigate accordingly
-          switch (user.AccessLevelID) {
-            case 1: // Student
-              this.router.navigate(['/user-menu/user-courses']);
-              break;
-            case 2: // Super Admin
-              this.router.navigate(['/menu/courses']);
-              break;
-            // Add more cases if you have additional access levels
-  
-            default:
-              // Handle unknown access levels or navigate to a default page
-              this.router.navigate(['']);
-              break;
+          if (user === 'not_approved') {
+            alert('Your account has not been approved yet. Please contact the administrator.');
+          } else {
+            alert('Welcome ' + user.FirstName + '. You are logged in.');
+            switch (user.AccessLevelID) {
+              case 1: // Student
+                this.router.navigate(['/user-menu/user-courses']);
+                break;
+              case 2: // Super Admin
+                this.router.navigate(['/menu/courses']);
+                break;
+              default:
+                this.router.navigate(['']);
+                break;
+            }
           }
         } else {
           alert('The login credentials you have entered are not correct.');
         }
       },
       (error) => {
+        // Handle other error cases
         console.error('Error during login:', error);
         alert('An error occurred during login.');
       }
